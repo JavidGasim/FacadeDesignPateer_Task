45d0e5ed586064de5e3f43d2412c3fa7049e5406
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-09 14:52:08 +0400

    Add files via upload

---
+++ FacadeDesignPattern_Task/FacadeDesignPattern_Task/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace FacadeDesignPattern_Task;
+
+class SubsystemA
+{
+    public void OperationA()
+    {
+        Console.WriteLine("Subsystem A - Operation A");
+    }
+}
+
+class SubsystemB
+{
+    public void OperationB()
+    {
+        Console.WriteLine("Subsystem B - Operation B");
+    }
+}
+
+class SubsystemC
+{
+    public void OperationC()
+    {
+        Console.WriteLine("Subsystem C - Operation C");
+    }
+}
+
+class Facade
+{
+    private SubsystemA subsystemA;
+    private SubsystemB subsystemB;
+    private SubsystemC subsystemC;
+
+    public Facade()
+    {
+        subsystemA = new SubsystemA();
+        subsystemB = new SubsystemB();
+        subsystemC = new SubsystemC();
+    }
+
+    public void Operation()
+    {
+        Console.WriteLine("\nFacade - Operation");
+        subsystemA.OperationA();
+        subsystemB.OperationB();
+        subsystemC.OperationC();
+    }
+}
+
+class Client
+{
+    static void Main()
+    {
+        // Facade oluştur
+        Facade facade = new Facade();
+
+        facade.Operation();
+
+        Console.ReadKey();
+    }
+}
